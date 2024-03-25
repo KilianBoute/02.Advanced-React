ee7f52381fb8b2384e5ac717da875b90b3d0bf22
Kilian Boute <kilian.boute@gmail.com>
2024-03-25 13:12:15 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-A mastermind game made in with React.js
-
-[Website link](https://kilianboute.github.io/02.Advanced-React/)
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <header>
-      <h1>Mastermind</h1>
-    </header>
-  );
-}
